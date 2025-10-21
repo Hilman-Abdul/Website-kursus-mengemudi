ae2b47282bb94fb15799c073247b5409c7e98461
Hilman-Abdul <hilmanganteng18@gmail.com>
2025-10-21 17:42:15 +0700

    initial commit

---
+++ database/migrations/2025_10_15_020053_create_peserta_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peserta', function (Blueprint $table) {
+            $table->id('id_peserta');
+            $table->string('Nama');
+            $table->string('Alamat');
+            $table->string('Tingkatan_Sabuk');
+            $table->integer('No_Reg');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peserta');
+    }
+};
